5013a752295b4408812da6019d2653490378e654
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-06 09:31:04 -0700

    Initial Commit

---
+++ UserChallenge/UserChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserChallengeApp.swift
+//  UserChallenge
+//
+//  Created by Garret Poole on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct UserChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
