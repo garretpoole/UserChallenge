3c0fd79e968b7f90dbc4cf87ba7f5dc6b4fcee4a
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-06 11:21:36 -0700

    loading users from json url and display name and active status

--- UserChallenge/ContentView.swift
+++ UserChallenge/ContentView.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var users: [User]
+}
+
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(users, id: \.name){ user in
+            VStack(alignment: .leading) {
+                Text(user.name)
+                    .font(.headline)
+                Text(user.isActive ? "Active" : "Not Active")
+            }
+        }
+        .task{
+            //'await' must be used for async functions
+            if(users.isEmpty){
+                print("loadData")
+                await loadData()
+            }
+        }
+    }
+    
+    func loadData() async{
+        print("loading...")
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")
+        else{
+            print("Invalid URL")
+            return
+        }
+        do {
+            //return is a tuple with data and metadata
+            let (data, _) = try await URLSession.shared.data(from: url)
+            //convert data object into Response
+            print(data.description)
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            if let decodedUsers = try? decoder.decode([User].self, from: data){
+                print("set users")
+                self.users = decodedUsers
+            }
+        } catch{
+            print("Invalid data")
+        }
+        print("Done")
+        print(users.isEmpty)
     }
 }
 
---
+++ UserChallenge/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  UserChallenge
+//
+//  Created by Garret Poole on 4/6/22.
+//
+
+import Foundation
+
+struct Friend: Codable{
+    let id: String
+    let name: String
+}
